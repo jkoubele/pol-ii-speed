76487c30566183e799667811d62f2b1d54030f1a
Koubele <koubele.jakub@gmail.com>
2025-05-23 19:02:10 +0200

    Add script to generate design matrix.

---
+++ scripts/generate_design_matrix.R
@@ -0,0 +1,30 @@
+library(argparse)
+library(tidyverse)
+
+parser <- ArgumentParser()
+parser$add_argument("--sample_annotation",
+                    help = ".tsv file with sample annotation",
+                    default = '/cellfile/datapublic/jkoubele/data_pol_ii/celegans_mutants/sample_annotation/sample_annotation.tsv')
+parser$add_argument("--output_folder",
+                    help = "",
+                    default = '/cellfile/datapublic/jkoubele/data_pol_ii/celegans_mutants/sample_annotation')
+args <- parser$parse_args()
+
+data <- read_tsv(args$sample_annotation)
+
+data <- data |> mutate(genotype = fct_relevel(as.factor(genotype), "wt"),
+                       age = fct_relevel(as.factor(age), "young"))
+
+formula <- ~genotype + age
+design_matrix <- model.matrix(formula, data)
+design_matrix <- design_matrix[, colnames(design_matrix) != "(Intercept)", drop = FALSE]
+
+rownames(design_matrix) <- data$sample_name
+
+df_out <- design_matrix |>
+  as.data.frame() |>
+  rownames_to_column(var = "sample_name")
+
+write_tsv(df_out, file.path(args$output_folder, 'design_matrix.tsv'))
+
+
