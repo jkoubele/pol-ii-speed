Fix scaling of different NLL components.

modelling/pol_ii_model.py
@@ -90,14 +90,14 @@ class CoverageLoss(nn.Module):
 
     def forward(self, phi, num_intronic_reads, coverage):
         loss_per_location = -torch.log(1 + phi.unsqueeze(1) - 2 * phi.unsqueeze(1) * self.locations)
-        return torch.mean(loss_per_location * coverage * num_intronic_reads.unsqueeze(1))
+        return torch.sum(loss_per_location * coverage * num_intronic_reads.unsqueeze(1))
 
 
 class Pol2TotalLoss(nn.Module):
     def __init__(self, num_position_coverage: int = 100):
         super().__init__()
-        self.loss_function_exon = nn.PoissonNLLLoss(log_input=True, full=True)
-        self.loss_function_intron = nn.PoissonNLLLoss(log_input=False, full=True)
+        self.loss_function_exon = nn.PoissonNLLLoss(log_input=True, full=True, reduction='sum')
+        self.loss_function_intron = nn.PoissonNLLLoss(log_input=False, full=True, reduction='sum')
         self.loss_function_coverage = CoverageLoss(num_position_coverage=num_position_coverage)
 
     def forward(self, gene_data: GeneData,
@@ -110,8 +110,8 @@ class Pol2TotalLoss(nn.Module):
                           for name in gene_data.intron_names)
 
         loss_coverage = sum(self.loss_function_coverage(phi[name],
-                                                        gene_data.intron_reads[name],
-                                                        gene_data.coverage_density[name])
+                            gene_data.intron_reads[name],
+                            gene_data.coverage_density[name])
                             for name in gene_data.intron_names)
 
         total_loss = loss_exon + loss_intron + loss_coverage

modelling/prepare_datasets.py
@@ -111,7 +111,7 @@ assert (annotation_df.index == intronic_reads_df.columns).all()
 
 design_matrix = torch.tensor(annotation_df['genotype'] == 'rp2', dtype=float).unsqueeze(1)
 library_size = torch.tensor(library_size_df['library_size_factor'])
-library_size = torch.ones_like(library_size)
+library_size = torch.ones_like(library_size) # TODO: Don't forget to uncomment to actually acount for difference in library size
 
 coverage_df_by_sample: dict[str, pd.DataFrame] = {}
 for sample_name in annotation_df.index:

modelling/train.py
@@ -50,7 +50,8 @@ def train_model(gene_data: GeneData,
     return model
 
 
-project_path = Path('/home/jakub/Desktop/dev-pol-ii-analysis/data/drosophila_mutants')
+# project_path = Path('/home/jakub/Desktop/dev-pol-ii-analysis/data/drosophila_mutants')
+project_path = Path('/cellfile/datapublic/jkoubele/data_pol_ii/drosophila_mutants/')
 with open(project_path / 'train_data' / 'data_train_with_solutions.pkl', 'rb') as file:
     data_train_with_solutions = pickle.load(file)
 
@@ -101,20 +102,20 @@ for name, size in zip(parameter_names, parameter_sizes):
     start += size
 total_params = start
 
-H_matrix = torch.zeros((total_params, total_params), device=device)
+hessian_matrix = torch.zeros((total_params, total_params), device=device)
 
 for name1, block_row in H.items():
     idx1_start, idx1_end = index_map[name1]
     for name2, block in block_row.items():
         idx2_start, idx2_end = index_map[name2]
-        H_matrix[idx1_start:idx1_end, idx2_start:idx2_end] = block.reshape(
+        hessian_matrix[idx1_start:idx1_end, idx2_start:idx2_end] = block.reshape(
             idx1_end - idx1_start, idx2_end - idx2_start
         )
 
-H_inv = torch.linalg.pinv(H_matrix).detach()
+hessian_matrix_inverse = torch.linalg.pinv(hessian_matrix).detach()
 
 # Standard errors (sqrt of diagonal elements)
-standard_errors = torch.sqrt(torch.diag(H_inv)).cpu().numpy()
+standard_errors = torch.sqrt(torch.diag(hessian_matrix_inverse)).cpu().numpy()
 
 # %%
 parameter_data: list[dict] = []
@@ -139,5 +140,5 @@ for name, size in zip(parameter_names, parameter_sizes):
 
 df_param = pd.DataFrame(data=parameter_data)
 df_param['SE'] = standard_errors
-df_param['z_value'] = df_param['value'] / standard_errors
-df_param['p_value'] = 2 * (1 - stats.norm.cdf(np.abs(df_param['z_value'])))
+df_param['z_score'] = df_param['value'] / standard_errors
+df_param['p_value'] = 2 * (1 - stats.norm.cdf(np.abs(df_param['z_score'])))