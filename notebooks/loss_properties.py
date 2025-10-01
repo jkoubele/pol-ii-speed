08ccab97b56d21978fcb2767a695a85813ff8609
Koubele <koubele.jakub@gmail.com>
2025-10-01 18:45:13 +0200

    WIP.

--- notebooks/loss_properties.py
+++ notebooks/loss_properties.py
@@ -29,6 +29,8 @@ with open("dataset_metadata.pkl", "rb") as file:
 #     log_output_folder=Path('/cellfile/datapublic/jkoubele/data_pol_ii/mouse_myocardium/results/logs'),
 #     gene_names_file_name='test.csv')
 #%%
+POISSON_LOSS_EPSILON = 1e-12
+MAX_GAP_TOLERANCE = 0.25
 
 gene_data = gene_data_list[0]
 design_matrix = dataset_metadata.design_matrix.numpy()
@@ -44,65 +46,64 @@ intercept_exon = cp.Variable()
 predicted_log_reads_exon = intercept_exon + design_matrix @ alpha
 exon_loss = cp.sum(cp.exp(predicted_log_reads_exon) - cp.multiply(exon_reads, predicted_log_reads_exon))
 
-
-#%% 1 intron only, ignore exon term
-
 beta = cp.Variable(p) # shape (p,)
 gamma = cp.Variable(p) # shape (p,)
 intercept_intron = cp.Variable()
 theta = cp.Variable()
 
-node_constraints = []
+#%% 1 intron only, ignore exon term
 
-example_row_0 = np.array([0.0], dtype=np.float32)
-example_row_1 = np.array([1.0], dtype=np.float32)
 
-node_constraints += [intercept_intron + theta - example_row_1 @ beta <= 1.0]
-node_constraints += [intercept_intron + example_row_1 @ gamma <= 1.0]
 # node_constraints = []
 
+# example_row_0 = np.array([0.0], dtype=np.float32)
+# example_row_1 = np.array([1.0], dtype=np.float32)
+
+# node_constraints += [intercept_intron + theta - example_row_1 @ beta <= 1.0]
+# node_constraints += [intercept_intron + example_row_1 @ gamma <= 1.0]
+# # node_constraints = []
 
 
-loss = 0
-problem_constraints = []
-problem_variables = {'u': [], 'a': [], 'b': []}
-POISSON_LOSS_EPSILON = 1e-12
-MAX_GAP_TOLERANCE = 0.25
 
-for i, row in enumerate(design_matrix):
-    a = intercept_intron + theta - row @ beta
-    b = intercept_intron + row @ gamma
-    # construct LP to get bounds on A and B   
-    min_a = cp.Problem(cp.Minimize(a), node_constraints).solve()
-    max_a = cp.Problem(cp.Maximize(a), node_constraints).solve()
+# loss = 0
+# problem_constraints = []
+# problem_variables = {'u': [], 'a': [], 'b': []}
+
+
+# for i, row in enumerate(design_matrix):
+#     a = intercept_intron + theta - row @ beta
+#     b = intercept_intron + row @ gamma
+#     # construct LP to get bounds on A and B   
+#     min_a = cp.Problem(cp.Minimize(a), node_constraints).solve()
+#     max_a = cp.Problem(cp.Maximize(a), node_constraints).solve()
     
-    min_b = cp.Problem(cp.Minimize(b), node_constraints).solve()
-    max_b = cp.Problem(cp.Maximize(b), node_constraints).solve()
+#     min_b = cp.Problem(cp.Minimize(b), node_constraints).solve()
+#     max_b = cp.Problem(cp.Maximize(b), node_constraints).solve()
     
-    # These min/max may not be tight for more complex constrains --> optimize v directly 
-    # in a convex program instead of the LPs above
-    min_v = np.exp(min_a) + np.exp(min_b)
-    max_v = np.exp(max_a) + np.exp(max_b)
+#     # These min/max may not be tight for more complex constrains --> optimize v directly 
+#     # in a convex program instead of the LPs above
+#     min_v = np.exp(min_a) + np.exp(min_b)
+#     max_v = np.exp(max_a) + np.exp(max_b)
     
-    n = intron_reads[i]
+#     n = intron_reads[i]
     
-    if max_v <= n:
-        loss += max_v - n * np.log(max_v + POISSON_LOSS_EPSILON)
-        print(f"{max_v} <= {n}")
-    else:
-        u = cp.Variable()
-        problem_constraints.append(u >= cp.exp(a) + cp.exp(b))
+#     if max_v <= n:
+#         loss += max_v - n * np.log(max_v + POISSON_LOSS_EPSILON)
+#         print(f"{max_v} <= {n}")
+#     else:
+#         u = cp.Variable()
+#         problem_constraints.append(u >= cp.exp(a) + cp.exp(b))
         
-        problem_variables['u'] += [u]
-        problem_variables['a'] += [a]
-        problem_variables['b'] += [b]
-        loss += u - n * cp.log(u + POISSON_LOSS_EPSILON)
-        pass
+#         problem_variables['u'] += [u]
+#         problem_variables['a'] += [a]
+#         problem_variables['b'] += [b]
+#         loss += u - n * cp.log(u + POISSON_LOSS_EPSILON)
+#         pass
     
     
-problem = cp.Problem(cp.Minimize(loss), node_constraints + problem_constraints)
-node_loss = problem.solve() # replace by a certified lower bound (dual objective) later on
-print(f"{node_loss=}")
+# problem = cp.Problem(cp.Minimize(loss), node_constraints + problem_constraints)
+# node_loss = problem.solve() # replace by a certified lower bound (dual objective) later on
+# print(f"{node_loss=}")
 
 #%% BnB
 
@@ -134,24 +135,57 @@ root_node = Node(parent_lower_bound=-np.inf,
 stack = PriorityQueue()
 stack.put(root_node)
 
+gaps = []
+i = 0
 while not stack.empty():
     node = stack.get()
+    print(f"{stack.qsize()=}")
+    print(50*'--')
+    
+    i += 1    
     
     loss = cp.Constant(0.0)
     problem_constraints: list[Constraint] = []
     lifted_variables = [cp.Variable() for _ in range(len(design_matrix))]
+    
+    node_is_infeasible = False
     for a, b, n, u in zip(logit_transcribing, logit_unspliced, intron_reads, lifted_variables):
         
         # Solve small LPs to get bounds on logits. Optionally solve full convex program
         # for predicted intron reads directly, which may be tighter that LPs for more complex constraints.
-        min_a = cp.Problem(cp.Minimize(a), node.node_constraints).solve()
-        max_a = cp.Problem(cp.Maximize(a), node.node_constraints).solve()
         
-        min_b = cp.Problem(cp.Minimize(b), node.node_constraints).solve()
-        max_b = cp.Problem(cp.Maximize(b), node.node_constraints).solve()        
+        problem_min_a = cp.Problem(cp.Minimize(a), node.node_constraints)
+        min_a = problem_min_a.solve(solver=cp.GLPK)
+          
+        
+        problem_max_a = cp.Problem(cp.Maximize(a), node.node_constraints)
+        max_a = problem_max_a.solve(solver=cp.GLPK)
+        
+        
+        problem_min_b = cp.Problem(cp.Minimize(b), node.node_constraints)
+        min_b = problem_min_b.solve(solver=cp.GLPK)
+             
+        
+        problem_max_b = cp.Problem(cp.Maximize(b), node.node_constraints)
+        max_b = problem_max_b.solve(solver=cp.GLPK)
+        
+        
+        if any([problem_min_a.status == cp.INFEASIBLE, 
+                problem_max_a.status == cp.INFEASIBLE,
+                problem_min_b.status == cp.INFEASIBLE,
+                problem_max_b.status == cp.INFEASIBLE]):
+            node_is_infeasible = True
+            break        
+        
+        # min_b = cp.Problem(cp.Minimize(b), node.node_constraints).solve()
+        # max_b = cp.Problem(cp.Maximize(b), node.node_constraints).solve()        
         
         min_v = np.exp(min_a) + np.exp(min_b)
         max_v = np.exp(max_a) + np.exp(max_b)  
+        
+        if max_v <= 0:
+            print(f"{max_v}")
+            assert False
         
         loss += u - n * cp.log(u + POISSON_LOSS_EPSILON)
         
@@ -161,9 +195,17 @@ while not stack.empty():
         else:
             problem_constraints += [u >= cp.exp(a) + cp.exp(b)]   
             
-    problem = cp.Problem(cp.Minimize(loss), node_constraints + problem_constraints)
+    print(f"{node_is_infeasible=}")
+    if node_is_infeasible:
+        continue    
+            
+    problem = cp.Problem(cp.Minimize(loss), node.node_constraints + problem_constraints)    
     loss_relaxed = problem.solve() # replace by a certified lower bound (dual objective) later on
     print(f"{loss_relaxed=}")
+    print(f"{problem.status=}")
+    
+    if problem.status == cp.INFEASIBLE:
+        continue
     
     if loss_relaxed >= best_feasible_loss:
         continue
@@ -175,7 +217,7 @@ while not stack.empty():
     print(f"{V.value=}")
     node_feasible_loss= sum(poisson_loss(V.value, intron_reads))
     
-    if node_feasible_loss < best_feasible_loss:
+    if node_feasible_loss + MAX_GAP_TOLERANCE< best_feasible_loss:
         print(f"{best_feasible_loss=}")
         best_feasible_loss = loss_relaxed
         print(f"{node_feasible_loss=}")
@@ -186,6 +228,9 @@ while not stack.empty():
         sample_loss_original = poisson_loss(V.value, intron_reads)
         sample_gap =  sample_loss_original - sample_loss_relaxed
         
+        print(f"{sample_gap.max()}")
+        gaps.append(sample_gap.max())
+        
         largest_gap_sample = int(sample_gap.argmax())
         x_for_bound = design_matrix[largest_gap_sample]
         bound_reads = (V.value[largest_gap_sample] + intron_reads[largest_gap_sample]) / 2
@@ -193,15 +238,34 @@ while not stack.empty():
         # Expand the current node
         cut_boundary_1 = intercept_intron + theta - x_for_bound @ beta - np.log(bound_reads / 2)
         cut_boundary_2 = intercept_intron + x_for_bound @ gamma -  np.log(bound_reads / 2)
-        children = []
-        for inequality_signs in [(1,1), (1,-1), (-1,1), (-1,-1)]:
-            new_constraints = [cut_boundary_1 * inequality_signs[0] <= 0,
-                               cut_boundary_2 * inequality_signs[1] <= 0]
-            child = Node(parent_lower_bound=loss_relaxed,
-                         node_constraints=node.node_constraints + new_constraints)
-            stack.put(child)
-            print(f"{child.node_constraints=}")
-    print(f"{stack.qsize()=}")
+        
+        cut_1 = [intercept_intron + theta - x_for_bound @ beta <=  np.log(bound_reads / 2),
+                 intercept_intron + x_for_bound @ gamma <=  np.log(bound_reads / 2)]
+        
+        cut_2 = [intercept_intron + theta - x_for_bound @ beta <=  np.log(bound_reads / 2),
+                 intercept_intron + x_for_bound @ gamma >=  np.log(bound_reads / 2)]
+        
+        cut_3 = [intercept_intron + theta - x_for_bound @ beta >=  np.log(bound_reads / 2),
+                 intercept_intron + x_for_bound @ gamma <=  np.log(bound_reads / 2)]
+        
+        cut_4 = [intercept_intron + theta - x_for_bound @ beta >=  np.log(bound_reads / 2),
+                 intercept_intron + x_for_bound @ gamma >=  np.log(bound_reads / 2)]
+        
+        for cut in [cut_1, cut_2,cut_3,cut_4]:
+            stack.put(Node(parent_lower_bound=loss_relaxed,
+                             node_constraints=node.node_constraints + cut))
+        
+        
+        # for inequality_signs in [(1,1), (1,-1), (-1,1), (-1,-1)]:
+        #     new_constraints = [cut_boundary_1 * inequality_signs[0] <= 0,
+        #                        cut_boundary_2 * inequality_signs[1] <= 0]
+        #     child = Node(parent_lower_bound=loss_relaxed,
+        #                  node_constraints=node.node_constraints + new_constraints)
+        #     stack.put(child)
+        #     print(f"{child.node_constraints=}")
+    
+    
+    
         
         
 
