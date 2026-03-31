71d3f4944dc8ca7d230def198810161fdbf3ae88
Koubele <koubele.jakub@gmail.com>
2026-03-31 16:53:25 +0200

    Small refactor of the script for coverage rescaling.

--- bin/compute_rescaled_coverage.R
+++ bin/compute_rescaled_coverage.R
@@ -26,26 +26,12 @@ parser$add_argument("--output_file_basename",
                     help = "Base name of the output file (without the extension).")
 parser$add_argument("--output_folder", help = "Folder to which the result will be saved.",
                     default = '.')
-parser$add_argument("--num_bins", 
+parser$add_argument("--num_bins",
                     default = 100,
                     type = "integer",
                     help = "Number of bins into which the coverage will be rescaled. ")
 
-if (interactive()){
-  args <- list(
-    bed_graph_plus='/cellfile/projects/pol_ii_speed/jkoubele/analysis/c_elegans_test/results/preprocessing/bed_graph_intron_coverage/K002000093_54873/coverage_plus.bedGraph.gz',
-    bed_graph_minus='/cellfile/projects/pol_ii_speed/jkoubele/analysis/c_elegans_test/results/preprocessing/bed_graph_intron_coverage/K002000093_54873/coverage_minus.bedGraph.gz',
-    introns_bed_file='/cellfile/projects/pol_ii_speed/jkoubele/analysis/c_elegans_test/results/preprocessing/genomic_features/introns.bed',
-    introns_count_file='/cellfile/projects/pol_ii_speed/jkoubele/analysis/c_elegans_test/results/preprocessing/intronic_reads/K002000093_54873/K002000093_54873.intron_read_counts.tsv',
-    output_file_basename='test_output',
-    output_folder='/cellfile/projects/pol_ii_speed/jkoubele/analysis/c_elegans_test/test_coverage_rescaling/',
-    num_bins = 100
-  )
-  
-} else {
-  args <- parser$parse_args()
-}
-
+args <- parser$parse_args()
 
 introns <- read_tsv(args$introns_bed_file,
                     col_names = c("chromosome", "start", "end", "name", "score", "strand")
@@ -65,29 +51,41 @@ intron_count <- read_tsv(
 )
 intron_count_lookup <- setNames(intron_count$count, intron_count$name)
 
-output_rescaled_coverages <- list()
+output_rescaled_coverages <- matrix(
+  0,
+  nrow = nrow(introns),
+  ncol = num_bins
+)
+rownames(output_rescaled_coverages) <- introns$name
 
 for (intron_number in seq_len(nrow(introns))) {
-  row <- introns[intron_number,]
+
+  chromosome <- introns$chromosome[intron_number]
+  start <- introns$start[intron_number]
+  end <- introns$end[intron_number]
+  strand <- introns$strand[intron_number]
+  intron_name <- introns$name[intron_number]
+
   if (intron_number %% 1000 == 0) {
     message(sprintf("[%s] Processed %d introns", Sys.time(), intron_number))
   }
 
-  if (row$strand == "+") {
+  if (strand == "+") {
     coverage_rle <- stats::window(
-      coverage_plus[[row$chromosome]],
-      start = row$start + 1, # Adding +1 to start since BED is 0-indexed (half-open) and window() is 1-indexed (closed interval)
-      end = row$end
+      coverage_plus[[chromosome]],
+      start = start + 1, # Adding +1 to start since BED is 0-indexed (half-open) and window() is 1-indexed (closed interval)
+      end = end
     )
-  } else if (row$strand == "-") {
+  } else if (strand == "-") {
     coverage_rle <- stats::window(
-      coverage_minus[[row$chromosome]],
-      start = row$start + 1, # Adding +1 to start since BED is 0-indexed (half-open) and window() is 1-indexed (closed interval)
-      end = row$end
+      coverage_minus[[chromosome]],
+      start = start + 1, # Adding +1 to start since BED is 0-indexed (half-open) and window() is 1-indexed (closed interval)
+      end = end
     )
   } else {
-    stop("Invalid strand: ", row$strand)
+    stop("Invalid strand: ", strand)
   }
+
 
   coverage <- as.numeric(coverage_rle)
   rescaled_coverage <- approx(
@@ -96,17 +94,20 @@ for (intron_number in seq_len(nrow(introns))) {
     xout = seq(1, length(coverage), length.out = num_bins)
   )$y
 
-  if (sum(rescaled_coverage) > 0) {
-    rescaled_coverage <- rescaled_coverage * intron_count_lookup[[row$name]] / sum(rescaled_coverage)
+  rescaled_sum <- sum(rescaled_coverage)
+
+  if (rescaled_sum > 0) {
+    rescaled_coverage <- rescaled_coverage * intron_count_lookup[[intron_name]] / rescaled_sum
   }
 
-  if (row$strand == "-") {
+  if (strand == "-") {
     rescaled_coverage <- rev(rescaled_coverage)
   }
 
-  output_rescaled_coverages[[row$name]] <- rescaled_coverage
+  output_rescaled_coverages[intron_number,] <- rescaled_coverage
+
 }
 
-df_out <- as_tibble(do.call(rbind, output_rescaled_coverages), rownames = "intron_name")
-colnames(df_out) <- c("intron_name", sprintf("coverage_bin_%03d", 1:num_bins))
+colnames(output_rescaled_coverages) <- sprintf("coverage_bin_%03d", 1:num_bins)
+df_out <- as_tibble(output_rescaled_coverages, rownames = "intron_name")
 write_parquet(df_out, file.path(args$output_folder, paste0(args$output_file_basename, ".parquet")))
